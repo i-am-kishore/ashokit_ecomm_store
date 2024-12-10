a50eaf7bc50baf34409c753d670e74b703c42216
i-am-kishore <kishorechary14@gmail.com>
2024-12-10 16:42:46 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a5a0029d2571e4f3495ab51500f19e55-1888408116.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://a78a58388278e4a85909ddc0369b96c3-488443332.ap-south-1.elb.amazonaws.com',
 };
